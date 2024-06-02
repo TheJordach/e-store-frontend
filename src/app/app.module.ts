40f2011191ef6ee01f21284725ebbbae057141ed
Jordach Makaya <makayajordach@gmail.com>
2024-06-02 01:38:54 +0100

    Refactoring: Create HomeComponent Â²&& make navigationCompent + HeaderComponent becbme child of newHomeCompnent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import {FontAwesomeModule} from '@fortawesome/angular-fontawesome';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { CatnavigationComponent } from './catnavigation/catnavigation.component';
+import {HomeModule} from "./home/home.module";
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
-    CatnavigationComponent
+
+
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FontAwesomeModule
+    HomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/home/home.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HomeComponent } from './home.component';
+import {HeaderComponent} from "./components/header/header.component";
+import {CatnavigationComponent} from "./components/catnavigation/catnavigation.component";
+import {FontAwesomeModule} from "@fortawesome/angular-fontawesome";
+
+
+
+@NgModule({
+  declarations: [
+    HomeComponent,
+    HeaderComponent,
+    CatnavigationComponent
+  ],
+  imports: [
+    CommonModule,
+    FontAwesomeModule,
+  ]
+})
+export class HomeModule { }
