ce487893e5d3ad56a0623f8904e9d151942e4c75
Jordach Makaya <makayajordach@gmail.com>
2024-06-02 01:13:56 +0100

    categorie navigation component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import {FontAwesomeModule} from '@fortawesome/angular-fontawesome';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { CatnavigationComponent } from './catnavigation/catnavigation.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    CatnavigationComponent
   ],
   imports: [
     BrowserModule,
